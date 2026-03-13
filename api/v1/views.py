20aece397c90a2476757eb9fdde8e8208abd2607
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-03-13 17:05:35 +0530

    [ADDED] API Versionong and API Docs

---
+++ api/v1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework import status, generics
+from api.models import User
+from .serializers import UserRegistrationSerializer
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = []
+ 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)   
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        
+        return Response({
+            'user': serializer.data,  
+            'refresh': str(refresh),  
+            'access': str(refresh.access_token),  
+        }, status=status.HTTP_201_CREATED)  
