4405b7ad8bef04dbc9fd31afaf97ffbf81478b1c
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-03-17 10:07:42 +0530

    [FIXED] Password Hashing issue

--- api/v1/serializers.py
+++ api/v1/serializers.py
@@ -7,19 +7,19 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'confirm_password', 
+        fields = ['username', 'email', 'password', 'confirm_password',
                   'first_name', 'last_name','phone_no','user_type',]
     
-        def validate(self, data):
-            if data['password'] != data['confirm_password']:
-                raise serializers.ValidationError("Passwords don't match")
-            return data
-        
-        def create(self, validated_data):
-            validated_data.pop('confirm_password')
-            user = User.objects.create_user(**validated_data)
-            # user.save()
-            return user
+    def validate(self, data):
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError("Passwords don't match")
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+        user = User.objects.create_user(**validated_data)
+        user.save()
+        return user
 
 class CustomerProfileSerializer(serializers.ModelSerializer):
     class Meta:
