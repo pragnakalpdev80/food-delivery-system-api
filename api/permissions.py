4a7cf88e06885d1610e8c939801abd3506830942
Hill Soni Dev80 <pragnakalp.dev80@gmail.com>
2026-03-18 15:52:12 +0530

    [ADDED] Custom Permissions

--- api/permissions.py
+++ api/permissions.py
@@ -1,23 +1,37 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-    # def has_object_permission(self, request, view, obj):
-    #     if request.method in permissions.SAFE_METHODS:
-    #         return True 
-    #     return obj.owner == request.user
-    pass
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True 
+        return obj.owner == request.user
 
 class IsRestaurantOwner(permissions.BasePermission):
-    pass
-
+    def has_permission(self, request, view):
+        return request.user.user_type == 'restaurant_owner'
+             
 class IsCustomer(permissions.BasePermission):
-    pass
-
+    def has_permission(self, request, view):
+        return request.user.user_type == 'customer'
+          
 class IsDriver(permissions.BasePermission):
-    pass
-
+    def has_permission(self, request, view):
+        return request.user.user_type == 'delivery_driver'
+    
 class IsOrderCustomer(permissions.BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        return obj.customer.user == request.user
 
 class IsRestaurantOwnerOrDriver(permissions.BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        is_restaurant_owner = (
+            request.user.user_type == 'restaurant_owner' and 
+            obj.restaurant.owner == request.user
+        )
+        
+        is_assigned_driver = (
+            request.user.user_type == 'delivery_driver' and
+            obj.driver.user == request.user
+        )
+        
+        return is_restaurant_owner or is_assigned_driver
